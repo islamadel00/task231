b35303fe770f94f9575d4e2466a354cf41546a98
Your Name <eslam.adel01999@gmail.com>
2025-09-04 14:16:12 +0300

    responsive

--- src/compounts/Nav/Nav.jsx
+++ src/compounts/Nav/Nav.jsx
@@ -18,6 +18,8 @@ function Nav() {
                         <a href="#"><i className="fa-solid fa-user"></i> LOGIN</a>
                         <a href="#"><i className="fa-solid fa-magnifying-glass"></i></a>
                     </div>
+                      {/* hamburger */}
+    <div class="hamburger">&#9776;</div>
                 </div>
             </nav>
         </>
